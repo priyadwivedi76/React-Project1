2945dd620bee54507609bc7699ca9d9f03ddd0eb
Dwivedi Priya <pd7668665@gmail.com>
2024-10-05 21:02:42 +0530

    Home-Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
 import React from 'react';
-
+import { Routes,Route } from 'react-router-dom';
+import Home from './components/Home';
 function App() {
-    return <div className="h-screen w-full">
-
-        {/* navigation */}
-    <nav className='w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5'>
-        <a className='px-5 py-2 border rounded border-blue-300 text-blue-500' href="/create">Add New Project</a>
-        <hr className='my-3 w-[80%]'/>
-        <h1 className='text-2xl mb-3 w-[80%]'>Category Filter</h1>
-        <ul className='w-[80%]'>
-            <li className="text-sm flex items-center mb-3">
-                <span className=' mr-2 w-[15px] h-[15px] rounded-full bg-blue-200'></span>
-                Category 1
-            </li> 
-            <li className="text-sm flex items-center mb-3">
-                <span className=' mr-2 w-[15px] h-[15px] rounded-full bg-red-200'></span>
-                Category 1
-            </li> 
-            <li className="text-sm flex items-center mb-3">
-                <span className=' mr-2 w-[15px] h-[15px] rounded-full bg-green-200'></span>
-                Category 1
-            </li> 
-            <li className="text-sm flex items-center mb-3">
-                <span className=' mr-2 w-[15px] h-[15px] rounded-full bg-emerald-200'></span>
-                Category 1
-            </li> 
-        </ul>
-    </nav>
-    </div>
+    return(
+        <div className="h-screen w-full flex">  
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Nav=()=>{
+    return(
+        <>
+            <nav className='w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5'>
+                <a className='px-5 py-2 border rounded border-blue-300 text-blue-500' href="/create">Add New Project</a>
+                <hr className='my-3 w-[80%]'/>
+                <h1 className='text-2xl mb-3 w-[80%]'>Category Filter</h1>
+                <ul className='w-[80%]'>
+                    <li className="text-sm flex items-center mb-3">
+                        <span className=' mr-2 w-[15px] h-[15px] rounded-full bg-blue-200'></span>
+                        Category 1
+                    </li> 
+                    <li className="text-sm flex items-center mb-3">
+                        <span className=' mr-2 w-[15px] h-[15px] rounded-full bg-red-200'></span>
+                        Category 1
+                    </li> 
+                    <li className="text-sm flex items-center mb-3">
+                        <span className=' mr-2 w-[15px] h-[15px] rounded-full bg-green-200'></span>
+                        Category 1
+                    </li> 
+                    <li className="text-sm flex items-center mb-3">
+                        <span className=' mr-2 w-[15px] h-[15px] rounded-full bg-emerald-200'></span>
+                        Category 1
+                    </li> 
+                </ul>
+            </nav> 
+        </>
+    )
+}
+
+export default Nav;
