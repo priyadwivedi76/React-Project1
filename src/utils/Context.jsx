af8361d89cedde132c5325cc0f0abd03230c2988
Dwivedi Priya <pd7668665@gmail.com>
2024-10-09 17:24:26 +0530

    comments

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -3,7 +3,10 @@ import React,{ useEffect, useState} from "react";
 import ProductContext from "./ProductContext";
 
 const Context=(props)=>{
+    // Initialize the product state as null
     const [product,setProduct]=useState(null);
+
+    // Fetch products on component mount using the getProducts function
     const getProducts=async()=>{
         try{
             const {data}=await axios("/products");
@@ -13,10 +16,12 @@ const Context=(props)=>{
         }
     }
 
+    // Fetch products on component update using the getProducts function if the category changes
     useEffect(()=>{
         getProducts();
     },[]);
 
+    // Return the product state as a context provider for other components to consume
     return(
         <ProductContext.Provider value={[product,setProduct]}>
             {props.children}
